182fedce30fd849a7ba51fed97e8869ab5343aa3
cputnam-a11y <chrlsvryptnm@gmail.com>
2024-07-20 20:21:45 -0500

    Added Crafting and Stonecutting functionality.

--- src/main/java/portableallays/util/AllayTickableItemRegistry.java
+++ src/main/java/portableallays/util/AllayTickableItemRegistry.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Optional;
 
 public class AllayTickableItemRegistry {
-    private static HashMap<Item, AllayTickProvider> providers = new HashMap<>();
+    private static final HashMap<Item, AllayTickProvider> providers = new HashMap<>();
     public static void register(Item item, AllayTickProvider provider) {
         providers.put(item, provider);
     }
