e4f2ec99b246ad1067b0d4920345532dbb7b945b
cputnam-a11y <chrlsvryptnm@gmail.com>
2024-07-20 20:27:34 -0500

    Add Allay Inventory Ticking in preparation for Furnace Block

--- src/main/java/portableallays/mixin/AllayEntityMixin.java
+++ src/main/java/portableallays/mixin/AllayEntityMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.AllayEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -11,17 +12,19 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import portableallays.screenhandler.AllayCraftingScreenHandler;
 import portableallays.screenhandler.ModScreenHandlers;
 import portableallays.tag.ModTags;
+import portableallays.util.AllayTickableItemRegistry;
 
 @SuppressWarnings("unused")
 @Mixin(AllayEntity.class)
 public abstract class AllayEntityMixin extends PathAwareEntity {
-
     protected AllayEntityMixin(EntityType<? extends PathAwareEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -35,12 +38,23 @@ public abstract class AllayEntityMixin extends PathAwareEntity {
     @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
     private void handleCraftingOnRightClick(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         AllayEntity $this = ((AllayEntity) (Object) this);
-        ItemStack mainHandStack = $this.getMainHandStack();
+        ItemStack mainHandStack = this.getMainHandStack();
         if (!mainHandStack.isIn(ModTags.ALLAY_VALID_CRAFTING_ITEMS) || player.isSneaking()) {
             return;
         }
         if (!player.getWorld().isClient)
             ModScreenHandlers.openCrafting(mainHandStack, (ServerPlayerEntity) player, $this);
         cir.setReturnValue(ActionResult.SUCCESS);
+    }
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void onItemTick(CallbackInfo ci) {
+        AllayTickableItemRegistry
+                .getProvider(this.getMainHandStack().getItem())
+                .ifPresent(
+                        provider -> provider.tick(
+                                this.getMainHandStack(),
+                                (AllayEntity) (Object) this
+                        )
+                );
     }
 }
