440ccb7e048a3315c3b0c1e2d76a08c0221a38ff
JaaiDead <98309228+JaaiDead@users.noreply.github.com>
2024-07-21 12:08:10 +0000

    might fix ???

--- src/main/java/portableallays/screenhandler/AllayShulkerBoxScreenHandler.java
+++ src/main/java/portableallays/screenhandler/AllayShulkerBoxScreenHandler.java
@@ -1,29 +1,29 @@
 package portableallays.screenhandler;
 
+import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.PiglinBrain;
 import net.minecraft.entity.passive.AllayEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ShulkerBoxScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
-import org.jetbrains.annotations.Nullable;
 
 public class AllayShulkerBoxScreenHandler extends ShulkerBoxScreenHandler {
-    private static final Text TITLE = Text.translatable("container.portableallays.shulkerbox");
+    private static final Text CONTAINER_NAME = Text.translatable("container.portableallays.shulkerbox");
     private final AllayEntity allay;
     @SuppressWarnings("unused")
     private final ItemStack shulkerBox;
 
     public AllayShulkerBoxScreenHandler(int syncId, PlayerInventory playerInventory, AllayEntity allay, ItemStack shulkerBox) {
-        super(syncId, playerInventory);
+        super(syncId, playerInventory, new SimpleInventory(3 * 9));
         this.allay = allay;
         this.shulkerBox = shulkerBox;
     }
@@ -34,16 +34,17 @@ public class AllayShulkerBoxScreenHandler extends ShulkerBoxScreenHandler {
     }
 
     public static void open(ServerPlayerEntity player, AllayEntity allay) {
-        ItemStack shulkerBox = new ItemStack(Items.SHULKER_BOX); // Replace with actual item from the Allay if needed
+        ItemStack shulkerBox = allay.getMainHandStack(); // Assuming Shulker Box is held in the main hand
         player.openHandledScreen(new NamedScreenHandlerFactory() {
+
             @Override
             public Text getDisplayName() {
-                return TITLE;
+                return CONTAINER_NAME;
             }
 
-           @Nullable
-            @Override
-            public ScreenHandler createMenu(int syncId, PlayerInventory playerInventory, PlayerEntity player) {
+        @Nullable
+        @Override
+        public ScreenHandler createMenu(int syncId, PlayerInventory playerInventory, PlayerEntity player) {
                 if (!(player instanceof ServerPlayerEntity))
                     return null;
                 player.incrementStat(Stats.OPEN_SHULKER_BOX);
