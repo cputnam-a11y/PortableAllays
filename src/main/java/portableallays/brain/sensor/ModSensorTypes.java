32e39780754d707210067fe2dd15661077ebc5a3
MdeKool <matthijsdekool@gmail.com>
2024-07-28 00:27:26 +0200

    Register hostile entity sensor

--- src/main/java/portableallays/brain/sensor/ModSensorTypes.java
+++ src/main/java/portableallays/brain/sensor/ModSensorTypes.java
@@ -11,6 +11,7 @@ import java.util.function.Supplier;
 public class ModSensorTypes {
     public static final SensorType<CookieSensor> COOKIE_SENSOR_SENSOR_TYPE = register("cookie_sensor", CookieSensor::new);
     public static final SensorType<AnvilSensor> ANVIL_SENSOR_SENSOR_TYPE = register("anvil_sensor", AnvilSensor::new);
+    public static final SensorType<HostileEntitiesSensor> HOSTILE_ENTITIES_SENSOR = register("hostile_entities_sensor", HostileEntitiesSensor::new);
 
     @SuppressWarnings("SameParameterValue")
     private static <U extends Sensor<?>> SensorType<U> register(String id, Supplier<U> factory) {
