3af3234094db19600e631ae9a0641c9b61e364ba
MdeKool <matthijsdekool@gmail.com>
2024-07-27 20:39:10 +0200

    Add anvil SensorType and MemoryModule to allay SensorTypes and MemoryModules

--- src/main/java/portableallays/brain/mixin/AllayEntityMixin.java
+++ src/main/java/portableallays/brain/mixin/AllayEntityMixin.java
@@ -17,20 +17,22 @@ import java.util.List;
 
 @Mixin(AllayEntity.class)
 public class AllayEntityMixin {
-    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableList;of(Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;)Lcom/google/common/collect/ImmutableList;"))
+    @ModifyExpressionValue(method = "<clinit>", remap = false, at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableList;of(Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;)Lcom/google/common/collect/ImmutableList;"))
     private static ImmutableList<SensorType<? extends Sensor<AllayEntity>>> addCustomModSensors(ImmutableList<SensorType<? extends Sensor<AllayEntity>>> original) {
         PortableAllays.LOGGER.info("Adding custom sensors");
         List<SensorType<? extends Sensor<AllayEntity>>> sensorList = new ArrayList<>(original);
         // add custom sensors here
         sensorList.add(ModSensorTypes.COOKIE_SENSOR_SENSOR_TYPE);
+        sensorList.add(ModSensorTypes.ANVIL_SENSOR_SENSOR_TYPE);
         return ImmutableList.copyOf(sensorList);
     }
-    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableList;of(Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;[Ljava/lang/Object;)Lcom/google/common/collect/ImmutableList;"))
+    @ModifyExpressionValue(method = "<clinit>", remap = false, at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableList;of(Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;[Ljava/lang/Object;)Lcom/google/common/collect/ImmutableList;"))
     private static ImmutableList<MemoryModuleType<?>> addCustomModMemoryModules(ImmutableList<MemoryModuleType<?>> original) {
         PortableAllays.LOGGER.info("Adding custom memory modules");
         List<MemoryModuleType<?>> memoryModuleList = new ArrayList<>(original);
         // add custom memory modules here
         memoryModuleList.add(ModMemoryModuleTypes.COOKIE);
+        memoryModuleList.add(ModMemoryModuleTypes.ANVIL);
         return ImmutableList.copyOf(memoryModuleList);
     }
 }
