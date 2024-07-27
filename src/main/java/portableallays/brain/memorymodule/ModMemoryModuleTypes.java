d6a9481b60a462887bc293bbaadb5f1abf327f4f
MdeKool <matthijsdekool@gmail.com>
2024-07-27 20:35:13 +0200

    Register MemoryModule for anvil location and anvil location sensor type

--- src/main/java/portableallays/brain/memorymodule/ModMemoryModuleTypes.java
+++ src/main/java/portableallays/brain/memorymodule/ModMemoryModuleTypes.java
@@ -6,12 +6,14 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import portableallays.PortableAllays;
 
 import java.util.Optional;
 
 public class ModMemoryModuleTypes {
     public static final MemoryModuleType<Boolean> COOKIE = register("cookie", Codec.BOOL);
+    public static final MemoryModuleType<BlockPos> ANVIL = register("anvil", BlockPos.CODEC);
     public static void init() {}
     private static <U> MemoryModuleType<U> register(String id) {
         return  Registry.register(Registries.MEMORY_MODULE_TYPE, Identifier.of(PortableAllays.MOD_ID, id), new MemoryModuleType<>(Optional.empty()));
--- src/main/java/portableallays/brain/sensor/ModSensorTypes.java
+++ src/main/java/portableallays/brain/sensor/ModSensorTypes.java
@@ -10,6 +10,8 @@ import java.util.function.Supplier;
 
 public class ModSensorTypes {
     public static final SensorType<CookieSensor> COOKIE_SENSOR_SENSOR_TYPE = register("cookie_sensor", CookieSensor::new);
+    public static final SensorType<AnvilSensor> ANVIL_SENSOR_SENSOR_TYPE = register("anvil_sensor", AnvilSensor::new);
+
     @SuppressWarnings("SameParameterValue")
     private static <U extends Sensor<?>> SensorType<U> register(String id, Supplier<U> factory) {
         return Registry.register(Registries.SENSOR_TYPE, Identifier.ofVanilla(id), new SensorType<>(factory));
