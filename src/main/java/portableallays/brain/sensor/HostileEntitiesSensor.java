2caea4e1a1051ab74ce1efcef740f46456d55142
MdeKool <matthijsdekool@gmail.com>
2024-07-28 00:26:45 +0200

    Sense hostile entities and find nearest

--- src/main/java/portableallays/brain/sensor/HostileEntitiesSensor.java
+++ src/main/java/portableallays/brain/sensor/HostileEntitiesSensor.java
@@ -1,2 +1,33 @@
-package portableallays.brain.sensor;public class HostileEntitiesSensor {
+package portableallays.brain.sensor;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.ai.brain.sensor.NearestLivingEntitiesSensor;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.passive.AllayEntity;
+import net.minecraft.server.world.ServerWorld;
+import portableallays.PortableAllays;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class HostileEntitiesSensor extends NearestLivingEntitiesSensor<AllayEntity> {
+    protected void sense(ServerWorld world, AllayEntity allay) {
+        super.sense(world, allay);
+        findNearestTarget(allay, (entity) -> entity instanceof HostileEntity)
+                .ifPresent(entity -> {
+                    allay.getBrain().remember(MemoryModuleType.NEAREST_ATTACKABLE, entity);
+                    PortableAllays.LOGGER.info("Detected hostile entity: {}", entity);
+                });
+    }
+
+    private static Optional<LivingEntity> findNearestTarget(AllayEntity allay, Predicate<LivingEntity> predicate) {
+        Stream<LivingEntity> var10000 = allay.getBrain().getOptionalRegisteredMemory(MemoryModuleType.MOBS).stream().flatMap(Collection::stream);
+        Objects.requireNonNull(allay);
+        return var10000.filter(predicate).findFirst();
+    }
 }
