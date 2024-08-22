24ba618c7287c1ef6066767e1de4f762297ce3ce
MdeKool <matthijsdekool@gmail.com>
2024-08-22 19:32:16 +0200

    Activity mixin debug logging

--- src/main/java/portableallays/brain/sensor/AnvilSensor.java
+++ src/main/java/portableallays/brain/sensor/AnvilSensor.java
@@ -2,21 +2,27 @@ package portableallays.brain.sensor;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.brain.Activity;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.sensor.Sensor;
 import net.minecraft.entity.passive.AllayEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import org.slf4j.Logger;
+import portableallays.PortableAllays;
 import portableallays.brain.memorymodule.ModMemoryModuleTypes;
 
 import java.util.Optional;
 import java.util.Set;
 
 public class AnvilSensor extends Sensor<AllayEntity> {
+    Logger LOGGER = PortableAllays.LOGGER;
 
     @Override
     protected void sense(ServerWorld world, AllayEntity allay) {
-        Optional<LivingEntity> optionalHostile = allay.getBrain().getOptionalRegisteredMemory(MemoryModuleType.NEAREST_ATTACKABLE);
+        LOGGER.info(String.valueOf(allay.getBrain().hasActivity(Activity.FIGHT)));
+        LOGGER.info(allay.getBrain().getPossibleActivities().toString());
+        Optional<LivingEntity> optionalHostile = allay.getBrain().getOptionalRegisteredMemory(MemoryModuleType.ATTACK_TARGET);
         if (optionalHostile.isEmpty()) {
             return;  // If no hostile entity nearby, don't look for anvils.
         }
