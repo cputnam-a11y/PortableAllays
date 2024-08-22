6cc3951eeac1dbfd9d5d6ab776e50f406a0028a8
MdeKool <matthijsdekool@gmail.com>
2024-08-22 19:39:39 +0200

    Add mixins for new activity

--- src/main/java/portableallays/brain/mixin/AllayBrainMixin.java
+++ src/main/java/portableallays/brain/mixin/AllayBrainMixin.java
@@ -2,11 +2,20 @@ package portableallays.brain.mixin;
 
 import com.google.common.collect.ImmutableList;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.ai.brain.Activity;
+import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.ai.brain.task.Task;
+import net.minecraft.entity.ai.brain.task.WalkTowardsPosTask;
 import net.minecraft.entity.passive.AllayBrain;
 import net.minecraft.entity.passive.AllayEntity;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import portableallays.PortableAllays;
+import portableallays.brain.memorymodule.ModMemoryModuleTypes;
 
 import java.util.ArrayList;
 
@@ -22,6 +31,23 @@ public class AllayBrainMixin {
     private static ImmutableList<Task<AllayEntity>> addCustomIdleActivities(ImmutableList<Task<AllayEntity>> original) {
         ArrayList<Task<AllayEntity>> mutList = new ArrayList<>(original);
         // add custom idle activities here
+        return ImmutableList.copyOf(mutList);
+    }
+
+    @Unique
+    private static void addFightActivities(Brain<AllayEntity> brain) {
+        brain.setTaskList(Activity.FIGHT, 0, ImmutableList.of(WalkTowardsPosTask.create(ModMemoryModuleTypes.ANVIL, 1, 2.0f)));
+    }
+
+    @Inject(method = "create", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/AllayBrain;addIdleActivities(Lnet/minecraft/entity/ai/brain/Brain;)V"))
+    private static void createCustomBrain(Brain<AllayEntity> brain, CallbackInfoReturnable<Brain<?>> cir) {
+        addFightActivities(brain);
+    }
+
+    @ModifyExpressionValue(method = "updateActivities", remap = false, at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableList;of(Ljava/lang/Object;)Lcom/google/common/collect/ImmutableList;"))
+    private static ImmutableList<Activity> resetCustomActivities(ImmutableList<Activity> original) {
+        ArrayList<Activity> mutList = new ArrayList<>(original);
+        mutList.add(Activity.FIGHT);
         return ImmutableList.copyOf(mutList);
     }
 }
